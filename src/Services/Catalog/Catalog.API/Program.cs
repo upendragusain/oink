1a413c12eb0cf06d53021580a2ff6bb0abb51e98
Upendra Gusain <upendra.gusain@gmail.com>
2020-05-30 13:39:18 +0100

    add serilog file log

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,19 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Catalog.API
 {
     public class Program
     {
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //get configuration from appsettings.json
+            var configuration = GetConfiguration();
+
+            Serilog.Log.Logger = CreateSerilogger(configuration);
+
+            try
+            {
+                Log.Information("Starting web host ({ApplicationContext})...", AppName);
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +38,23 @@ namespace Catalog.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+
+        private static Serilog.ILogger CreateSerilogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .WriteTo.Seq(configuration["Serilog:SeqServerUrl"])
+                .WriteTo.File("logs.json")
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
     }
 }
